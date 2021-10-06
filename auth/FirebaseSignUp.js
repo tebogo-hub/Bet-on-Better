fccc080ccbe1c816e45a58e1aff447ae431bb4d6
lutho362 <lutho470@gmail.com>
2021-10-06 16:19:20 +0200

    m

--- auth/FirebaseSignUp.js
+++ auth/FirebaseSignUp.js
@@ -1,11 +1,13 @@
-import { firebase } from "../config"
+import { firebase } from "../config/firebase"
 
 const FirebaseSignUp =((name, email, password)=>{
     let datab = firebase.firestore()
     firebase.auth() .createUserWithEmailAndPassword(name, email, password)
     .then((userCredential) => {
       datab.collection("UserCreds").doc(email).set({name,email,password})
-})
+      console.log('clicked')
+}).catch(err =>
+  console.log(err))
 })
 
 export {FirebaseSignUp}
